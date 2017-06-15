e1118ddfb4a02258f60ad1e7cf7d6427e142573a
michielite <michimailneu@gmail.com>
2017-06-15 18:48:14 -0400

    my commit

--- hangman.py
+++ hangman.py
@@ -2,8 +2,15 @@ from __future__ import print_function
 words= ("Tree", "Virus", "Hangman", "Ebola", "Fun")
 import numpy
 word=(numpy.random.choice(words))
+s=word
 print(len(word))
+letter=raw_input("Please enter a single letter to guess the word!")
 letter = ""
+letter=y
 while len(letter)!= 1:
-        letter=raw_input("Please enter a single letter to guess the word!")
-print(letter)
+    if letter in word:
+        print(y)
+        break
+    else:
+        print("Letter was incorrect")
+        break
