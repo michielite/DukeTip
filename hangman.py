a503b71999a95e0b3b2795d5accf904e6ef45b7f
michielite <michimailneu@gmail.com>
2017-06-15 15:43:54 -0400

    my commit

--- hangman.py
+++ hangman.py
@@ -1,3 +1,7 @@
-from__future__import print_function
-words= (Tree, Virus, Hangman, Ebola, Fun)
+from __future__ import print_function
+words= ("Tree", "Virus", "Hangman", "Ebola", "Fun")
+import numpy
+print(numpy.random.choice(words))
+letter=raw_input("Please enter a single letter to guess the word!")
+
 print(words)
