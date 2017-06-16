96ff3e7415b95f553ccc032131303eb51b525c14
michielite <michimailneu@gmail.com>
2017-06-16 11:45:08 -0400

    my commit

--- hangman.py
+++ hangman.py
@@ -1,33 +1,67 @@
 from __future__ import print_function
 words= ("Tree", "Virus", "Hangman", "Ebola", "Fun")
-man1='''
+man='''
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+========''',
+'''
+   +---+
+   |   |
+   O   |
+   |   |
+       |
+       |
+=======''',
+'''
+   +---+
+   |   |
+   O   |
+  /|\  |
+       |
+       |
+=======''',
+'''
+   +---+
+   |   |
+   O   |
+  /|\  |
+  /    |
+       |
+=======''',
+'''
+   +---+
+   |   |
+   O   |
+  /|\  |
+  / \  |
+       |
+======='''
 import numpy
-print(man1)
 word = (numpy.random.choice(words))
 s = word
 print(len(word))
-WRONG=()
-numberWrong=len(WRONG)
 while True:
-    letter = raw_input("Please enter a single letter to guess the word!")
-    klo= letter
-    while len(letter) == 1:
-        if klo in s:
-            print("correct")
-            break
+    WRONG=()
+    numberWrong=len(WRONG)
+    if numberWrong == 0:
+        print(man[0])
+        break
+    else:
+        if numberWrong == 1:
+            print(man[1])
         else:
-            print("Letter" letter "was incorrect")
-            break
-    def printHangman(numberWrong):
-        if numberWrong >=1:
-            print("head")
-        else:
-
-            if numberWrong >= 2:
-                print("body")
+            if numberWrong >= 3:
+                print("arms")
             else:
-                if numberWrong >= 3:
-                    print("arms")
-                else:
-                    print("You are correct")
-
+                print("You are Dead")
+letter = raw_input("Please enter a single letter to guess the word!")
+klo= letter
+while len(letter) == 1:
+    if klo in s:
+        print("correct")
+    else:
+        print("Letter was incorrect")
